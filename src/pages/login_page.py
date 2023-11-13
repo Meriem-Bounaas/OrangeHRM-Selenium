27a80aaab6cea149348e2067a780b1c685e82788
meriem <bouunaas.meriem.1993@gmail.com>
2023-11-13 20:57:35 +0100

    add PageFactory and fixing locators

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -1,29 +1,24 @@
 from selenium.webdriver.common.by import By
 from utils.common import find_element
+from seleniumpagefactory.Pagefactory import PageFactory
 
 
-class LoginPage:
+class LoginPage(PageFactory):
     def __init__(self, driver) -> None:
         self.driver = driver
 
     locators = {
         'username': ("NAME", "username"),
-        'password': ("NAME ", "password"),
-        'loginbtn': ("TAG_NAME ", "button")
+        'password': ("NAME", "password"),
+        'loginbtn': ("XPATH", '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button')
     }
 
     def login(self, email: str, psw: str) -> None:
-        self.driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        self.driver.get(
+            'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         self.username.set_text(email)
         self.password.set_text(psw)
         self.loginbtn.click_button()
-        # username = self.driver.find_element(By.NAME, 'username')
-        # username.send_keys(email)
-        # password = self.driver.find_element(By.NAME, 'password')
-        # password.send_keys(email)
-        # loginbtn = self.driver.find_element(By.TAG_NAME, 'button')
-        # loginbtn.click()
-
 
     def verify_page_title(self) -> bool:
         return self.driver.title == 'OrangeHRM'
